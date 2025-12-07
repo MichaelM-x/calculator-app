b551e23dd8151b5c1b37fa9cd068ff1586574858
MichaelMurwayix <michael.murwayix@gmail.com>
2025-12-07 15:14:16 +0300

    adding multiply function test file

--- backend/tests/multiply.test.js
+++ backend/tests/multiply.test.js
@@ -1,19 +1,19 @@
 const request = require("supertest");
 const app = require("../app"); // Assuming your Express app is exported from app.js
 
-describe("POST/subtract", () => {
-  it("returns Correct Difference of two numbers", async () => {
+describe("POST/multiply", () => {
+  it("returns Correct Product of two numbers", async () => {
     const res = await request(app)
-      .post("/api/subtract")
-      .send({ input1: 15, input2: 5 });
+      .post("/api/multiply")
+      .send({ input1: 4, input2: 5 });
 
     expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty("difference", 10);
+    expect(res.body).toHaveProperty("product", 20);
   });
   it("handles invalid inputs", async () => {
     const res = await request(app)
-      .post("/api/subtract")
-      .send({ input1: "fifteen", input2: 5 });
+      .post("/api/multiply")
+      .send({ input1: "four", input2: 5 });
 
     expect(res.statusCode).toEqual(400);
   });
